d61d632697a9c9d3e93d582b7337e0313c149681
pawanman56 <pawanman56@gmail.com>
2020-03-15 01:52:32 +0545

    react-native-meteor added

--- src/main.vue
+++ src/main.vue
@@ -8,9 +8,11 @@
 <script>
 import Vue from 'vue-native-core';
 import { VueNativeBase } from 'native-base';
+import Meteor from 'react-native-meteor';
 import Index from './index';
 
 Vue.use(VueNativeBase);
+Meteor.connect('ws://192.168.42.82:3000/websocket');
 
 export default {
   components: { Index }
