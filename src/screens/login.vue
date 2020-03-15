b233f9a674edb165645bb4d7cb9eb07df1d49588
pawanman56 <pawanman56@gmail.com>
2020-03-15 10:17:01 +0545

    login page added

--- src/screens/login.vue
+++ src/screens/login.vue
@@ -10,14 +10,14 @@
     <nb-content padder>
       <nb-form>
           <nb-item>
-            <nb-input placeholder="Email"/>
+            <nb-input placeholder="Email" v-model="emailValue" auto-capitalize="none"/>
           </nb-item>
           <nb-item last>
-            <nb-input placeholder="Password"/>
+            <nb-input placeholder="Password" secure-text-entry v-model="password" auto-capitalize="none"/>
           </nb-item>
         </nb-form>
         <view :style="{marginTop:10}">
-          <nb-button block>
+          <nb-button block :on-press='login'>
             <nb-text>Login</nb-text>
           </nb-button>
         </view>
@@ -28,6 +28,8 @@
 </template>
 
 <script>
+import Meteor, { createContainer } from 'react-native-meteor';
+
 export default {
   data: function() {
     return {
@@ -42,6 +44,15 @@ export default {
 
   methods: {
     login() {
+      if (this.emailValue && this.password) {
+        Meteor.loginWithPassword(this.emailValue.toLowerCase(), this.password, error => {
+          if (error) {
+            alert('Incorrect Email or Password.');
+          }
+        });
+      } else {
+        alert('Please enter existing email and password.');
+      }
     }
   }
 };
